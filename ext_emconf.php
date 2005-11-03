079f886945690933dc7e76c4b1a74ee867b549ac
andreas <andreas@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-11-03 09:35:39 +0000

    Extension is not shy anymore.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dkd_tools/trunk@589 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,9 +2,9 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "dkd_tools"
-# 
+#
 # Auto generated 01-11-2005 16:58
-# 
+#
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
 ########################################################################
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'category' => 'module',
 	'author' => 'Andreas Otto',
 	'author_email' => 'andreas.otto@dkd.de',
-	'shy' => 1,
+	'shy' => 0,
 	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'author_company' => 'd.k.d Internet Service GmbH, www.dkd.de',
 	'private' => '',
 	'download_password' => '',
-	'version' => '1.0.2',	// Don't modify this! Managed automatically during upload to repository.
+	'version' => '1.0.3',	// Don't modify this! Managed automatically during upload to repository.
 	'_md5_values_when_last_written' => 'a:11:{s:12:"ext_icon.gif";s:4:"1f3e";s:17:"ext_localconf.php";s:4:"d8f6";s:14:"ext_tables.php";s:4:"56d8";s:19:"doc/wizard_form.dat";s:4:"ea33";s:20:"doc/wizard_form.html";s:4:"2422";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"654b";s:14:"mod1/index.php";s:4:"f18d";s:18:"mod1/locallang.php";s:4:"fd2c";s:22:"mod1/locallang_mod.php";s:4:"c895";s:19:"mod1/moduleicon.gif";s:4:"1f3e";}',
 );
 
